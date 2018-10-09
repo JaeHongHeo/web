c797bb79729b479468ee9e0114b9512db821d991
dicapriheo <hitakuri@naver.com>
2018-10-09 23:31:01 +0900

    service worker 테스트

---
+++ main.js
@@ -0,0 +1,16 @@
+/**
+서비스 워커
+https://gracefullight.github.io/2017/12/22/PWA-ServiceWorker-Web-Caching/
+ */
+
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('/static/js/sw.js').then(function(reg) {
+    console.log(':^)', reg);
+    // TODO
+
+    }).catch(function(err) {
+        console.log(':^(', err);
+    });
+}
+
---
+++ sw.js
@@ -0,0 +1,30 @@
+
+/*
+https://gracefullight.github.io/2017/12/22/PWA-ServiceWorker-Web-Caching/
+*/
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/main.js'
+];
+
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  // TODO
+});
