e4a80d328b94e346f6e671c162622d4e2cff8b42
dicapriheo <hitakuri@naver.com>
2018-10-09 23:40:59 +0900

    change js paths

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ https://gracefullight.github.io/2017/12/22/PWA-ServiceWorker-Web-Caching/
 
 if ('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
-    navigator.serviceWorker.register('/static/js/sw.js').then(function(reg) {
+    navigator.serviceWorker.register('/web/sw.js').then(function(reg) {
     console.log(':^)', reg);
     // TODO
 
