150b198bc4f6d4f90cbe05478af60c6bf46eb696
dicapriheo <hitakuri@naver.com>
2018-10-10 22:42:08 +0900

    푸시 권한 요청

--- main.js
+++ main.js
@@ -41,3 +41,11 @@ messaging.usePublicVapidKey('BKkhjdbpZ1KWj6PtPB9dJ-uqC8NATwf40fi0ORcCbAYIJSW7nid
     console.log('Message received. ', payload);
   });
 
+
+messaging.requestPermission().then(function() {
+  console.log('Notification permission granted.');
+  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+  // ...
+}).catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
