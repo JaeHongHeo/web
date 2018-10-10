be3ad4d4a9b560616d509f8215e6dbf96533b78d
dicapriheo <hitakuri@naver.com>
2018-10-10 23:10:35 +0900

    index.html : firebase-messaging.js 추가

--- main.js
+++ main.js
@@ -3,11 +3,22 @@
 https://gracefullight.github.io/2017/12/22/PWA-ServiceWorker-Web-Caching/
  */
 
-if ('serviceWorker' in navigator) {
-    console.log('Service Worker is supported');
-    navigator.serviceWorker.register('/web/sw.js').then(function(reg) {
-    console.log(':^)', reg);
-    // TODO
+//if ('serviceWorker' in navigator) {
+//    console.log('Service Worker is supported');
+//    navigator.serviceWorker.register('/web/sw.js').then(function(reg) {
+//    console.log(':^)', reg);
+//    // TODO
+//
+//    }).catch(function(err) {
+//        console.log(':^(', err);
+//    });
+//}
+
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey('BKkhjdbpZ1KWj6PtPB9dJ-uqC8NATwf40fi0ORcCbAYIJSW7nid7ndCXbUt1H3u7bCF32eqSFCixSApXvSMAS20');
+
+
     // test
     messaging.getToken().then(function(refreshedToken) {
       console.log('getToken. ' + refreshedToken);
@@ -15,16 +26,6 @@ if ('serviceWorker' in navigator) {
       console.log('getToken fail', err);
       //showToken('Unable to retrieve refreshed token ', err);
     });
-
-    }).catch(function(err) {
-        console.log(':^(', err);
-    });
-}
-
-
-const messaging = firebase.messaging();
-messaging.usePublicVapidKey('BKkhjdbpZ1KWj6PtPB9dJ-uqC8NATwf40fi0ORcCbAYIJSW7nid7ndCXbUt1H3u7bCF32eqSFCixSApXvSMAS20');
-
 
   // IDs of divs that display Instance ID token UI or request permission UI.
   const tokenDivId = 'token_div';
